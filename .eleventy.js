aada57c0fb524fadbbb2eba80f8b390f65be4c3c
Ariyan <a.abidi150@gmail.com>
2021-07-03 03:50:51 +0330

    bug fix

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig){
     
     eleventyConfig.addPassthroughCopy('./src/styles')
     eleventyConfig.addPassthroughCopy('./src/assets')
+    eleventyConfig.addPassthroughCopy('./src/admin')
     
     return {
         dir: {
