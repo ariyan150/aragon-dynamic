cd718fc6ee31a45740e1e14305bd7b494f2e3f53
Ariyan <a.abidi150@gmail.com>
2021-07-03 05:34:02 +0330

    category12

--- src/scripts/filter-video.js
+++ src/scripts/filter-video.js
@@ -1,15 +1,15 @@
 let sortBtnvid = document.querySelector('.filter-menu.video').children;
 let sortItemvid = document.querySelectorAll('.video_card');
 
-for(let k = 0; k < sortItemvid.length; k++) {
-    sortItemvid[k].classList.remove('avtive');
-    sortItemvid[k].classList.add('delete');
+// for(let k = 0; k < sortItemvid.length; k++) {
+//     sortItemvid[k].classList.remove('avtive');
+//     sortItemvid[k].classList.add('delete');
 
-    if(sortItemvid[k].getAttribute('data-item') == 'montakhab'){
-        sortItemvid[k].classList.remove('delete');
-        sortItemvid[k].classList.add('active');
-    }
-}
+//     if(sortItemvid[k].getAttribute('data-item') == 'montakhab'){
+//         sortItemvid[k].classList.remove('delete');
+//         sortItemvid[k].classList.add('active');
+//     }
+// }
 
 for (let i = 0; i < sortBtnvid.length; i++) {
     sortBtnvid[i].addEventListener('click', function() {
